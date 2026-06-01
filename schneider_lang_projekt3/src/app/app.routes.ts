0fae094fe9c3ac16af8704939c7befcc30d62d91
Sebastian Schneider <lvl1potato69@gmail.com>
2026-06-01 10:32:44 +0200

    ls-13 nachgeholt - eingeloggt bleiben

--- schneider_lang_projekt3/src/app/app.routes.ts
+++ schneider_lang_projekt3/src/app/app.routes.ts
@@ -1,10 +1,20 @@
+import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import { Liste } from './liste/liste';
-import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: Login },
   { path: 'register', component: Register },
-  { path: 'liste', component: Liste },
+  { path: 'liste', component: Liste, canActivate: [authGuard] },
 ];
+
+export function authGuard(): boolean {
+  const user = localStorage.getItem('loggedInUser');
+  if (user) {
+    return true;
+  } else {
+    window.location.href = '/';
+    return false;
+  }
+}
