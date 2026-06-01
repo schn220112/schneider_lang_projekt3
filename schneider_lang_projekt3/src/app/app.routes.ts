1fe8f2d5ac0ad6c7b5f8c1284e109ff01bdc0643
Sebastian Schneider <lvl1potato69@gmail.com>
2026-06-01 10:09:33 +0200

    app.html & login route update

--- schneider_lang_projekt3/src/app/app.routes.ts
+++ schneider_lang_projekt3/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'register', component: Register },
+];
