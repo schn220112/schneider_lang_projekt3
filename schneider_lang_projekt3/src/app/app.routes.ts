f1576e9bf50e2b69ec7fa602ca978019e4e97485
Sebastian Schneider <lvl1potato69@gmail.com>
2026-06-01 10:29:12 +0200

    liste app route korrektur

--- schneider_lang_projekt3/src/app/app.routes.ts
+++ schneider_lang_projekt3/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { Liste } from './liste/liste';
 import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: Login },
   { path: 'register', component: Register },
+  { path: 'liste', component: Liste },
 ];
