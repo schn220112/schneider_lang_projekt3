209367d709a44cc60d29d892a144d6e991f1b6f6
Sebastian Schneider <lvl1potato69@gmail.com>
2026-06-01 09:57:50 +0200

    ordner update liste und details

---
+++ schneider_lang_projekt3/src/app/liste/liste.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste',
+  imports: [],
+  templateUrl: './liste.html',
+  styleUrl: './liste.css',
+})
+export class Liste {
+
+}
