d0b231b9b5b2ae7afaa3d9a948ffee7d097af9da
Sebastian Schneider <lvl1potato69@gmail.com>
2026-06-01 10:13:48 +0200

    app.html & login fehler korrektur

--- schneider_lang_projekt3/src/app/login/login.ts
+++ schneider_lang_projekt3/src/app/login/login.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    const name     = (document.getElementById('loginName') as HTMLInputElement).value;
+    const password = (document.getElementById('loginPassword') as HTMLInputElement).value;
+
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+    const user  = users.find((u: any) => u.name === name && u.password === password);
+
+    if (user) {
+      localStorage.setItem('loggedInUser', JSON.stringify(user));
+      this.router.navigate(['/liste']);
+    } else {
+      const err = document.getElementById('loginError')!;
+      err.style.display = 'block';
+    }
+  }
 }
