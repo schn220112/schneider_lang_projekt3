d414bb28dca7c8ba1f9f1fb43e0031f23c298fec
Sebastian Schneider <lvl1potato69@gmail.com>
2026-06-01 12:22:15 +0200

    ls-17 - eingeloggt bleiben auf firebase anpassen + toggle button

--- schneider_lang_projekt3/src/app/login/login.ts
+++ schneider_lang_projekt3/src/app/login/login.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, onAuthStateChanged, setPersistence, browserLocalPersistence, browserSessionPersistence } from 'firebase/auth';
 import { auth } from '../firebase';
 import {FormsModule} from '@angular/forms';
 
@@ -10,18 +10,31 @@ import {FormsModule} from '@angular/forms';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit {
 
   constructor(private router: Router) {}
 
-  async onSubmit() {
-    const email    = (document.getElementById('loginName') as HTMLInputElement).value.trim();
-    const password = (document.getElementById('loginPassword') as HTMLInputElement).value;
-    const errorMsg = document.getElementById('loginError')!;
+  ngOnInit() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        window.location.href = '/liste';
+      }
+    });
+  }
+
+  async onSubmit(event: Event) {
+    event.preventDefault();
+
+    const email      = (document.getElementById('loginName') as HTMLInputElement).value.trim();
+    const password   = (document.getElementById('loginPassword') as HTMLInputElement).value;
+    const rememberMe = (document.getElementById('rememberMe') as HTMLInputElement).checked;
+    const errorMsg   = document.getElementById('loginError')!;
 
     try {
+      // Eingeloggt bleiben = Local, sonst nur für diese Session
+      await setPersistence(auth, rememberMe ? browserLocalPersistence : browserSessionPersistence);
       await signInWithEmailAndPassword(auth, email, password);
-      this.router.navigate(['/liste']);
+      window.location.href = '/liste';
     } catch (e) {
       errorMsg.style.display = 'block';
     }
